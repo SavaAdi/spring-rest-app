d23a247bcca661b9299551a2ee33f959509bdd74
SavaAdi <50299766+SavaAdi@users.noreply.github.com>
2019-11-16 12:32:15 +0200

    pushing project

---
+++ src/test/java/com/adisavaspring/rest/springrestapp/SpringRestAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.adisavaspring.rest.springrestapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringRestAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
